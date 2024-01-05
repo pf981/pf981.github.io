12bd3468c1ab3ecf6fc9b696a876c1175cc0a3fc
Paul Foster <paulfstr@gmail.com>
2024-01-05 19:18:20 +1100

    Update solution

--- posts/2023/03/solution.py
+++ posts/2023/03/solution.py
@@ -1,61 +1,28 @@
-import dataclasses
 import math
+import re
 
 
-with open('input.txt', "r") as f:
+with open(r'input.txt', "r") as f:
     lines = f.readlines()
 
 
-@dataclasses.dataclass(frozen=True)
-class Number:
-    row: int
-    start_col: int
-    end_col: int
-    value: int
-
-
-def get_number(pos, digits):
-    start_col = pos[1]
-    while (pos[0], start_col - 1) in digits:
-        start_col -= 1
-
-    end_col = pos[1]
-    while (pos[0], end_col + 1) in digits:
-        end_col += 1
-
-    return Number(
-        row=pos[0],
-        start_col=start_col,
-        end_col=end_col,
-        value=int(''.join(digits[pos[0], col] for col in range(start_col, end_col + 1)))
-    )
-
-
-m = {(row, col): c for row, line in enumerate(lines) for col, c in enumerate(line)}
-symbols = {pos for pos, c in m.items() if c not in '0123456789.'}
-digits = {pos: c for pos, c in m.items() if c in '0123456789'}
-
 numbers = {}
-for pos, c in digits.items():
-    if pos in numbers:
-        continue
+symbols = {}
+for row, line in enumerate(lines):
+    for match in re.finditer(r'\d+', line):
+        for col in range(match.start(), match. end()):
+            numbers[(row, col)] = match
+    for match in re.finditer(r'[^0-9.\n]', line):
+        symbols[(row, match.start())] = [match.group()]
 
-    number = get_number(pos, digits)
-    for col in range(number.start_col, number.end_col + 1):
-        numbers[(number.row, col)] = number
+for (row, col), l in symbols.items():
+    neighbours = {numbers[(row + dr, col + dc)] for dr in range(-1, 2) for dc in range(-1, 2) if (row + dr, col + dc) in numbers}
+    l.append([int(match.group()) for match in neighbours])
 
-valid_numbers = {numbers[(row + dr, col + dc)] for row, col in symbols for dr in range(-1, 2) for dc in range(-1, 2) if (row + dr, col + dc) in numbers}
-answer1 = sum(number.value for number in valid_numbers)
+answer1 = sum(num for _, neighbours in symbols.values() for num in neighbours)
 print(answer1)
 
 
 # Part 2
-gears = {pos for pos, c in m.items() if c == '*'}
-gear_ratios = 0
-for row, col in gears:
-    neighbors = {numbers[row + dr, col + dc] for dr in range(-1, 2) for dc in range(-1, 2) if (row + dr, col + dc) in numbers}
-    if len(neighbors) == 2:
-        gear_ratios += math.prod(number.value for number in neighbors)
-
-answer2 = gear_ratios
+answer2 = sum(math.prod(nums) for symbol, nums in symbols.values() if symbol == '*' and len(nums) == 2)
 print(answer2)
